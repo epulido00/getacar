0a6dbcda2018f09b98e298031aa009187f10d9ec
Edwin Pulido <epulido@MacBook-Pro-de-Edwin.local>
2021-11-08 14:10:36 -0600

    reestructuracion

---
+++ getacar/settings/prod.py
@@ -0,0 +1,29 @@
+from .base import *
+import dj_databaase_url
+from decouple import config
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_databaase_url.config(
+        default = config('DATABASE_URL')
+    )
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
